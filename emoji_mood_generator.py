a1b4df1725971d366b37b9771b10e7a9a6fbcfe6
mi9Ans <anshumaanmishra@myyahoo.com>
2025-07-05 19:14:15 +0530

    Added emoji_mood_generator.py - mood-based emoji responder
    ### emoji_mood_generator.py
    A creative and interactive program that asks the user how they're feeling and responds with emojis and short messages based on their mood. It uses conditional logic, string input, and emoji support.

---
+++ emoji_mood_generator.py
@@ -0,0 +1,19 @@
+#Enter your feeling
+i = (input("How are you feeling :")).lower()
+
+#Respond based on feeling 
+if i == "happy":
+    print("😁 I am feeling happy")
+elif i == "sad":
+    print("🙁 I'm sad")
+elif i == "Cool":
+    print("😎 I'm Cool")
+elif i == "angry":
+    print("😡 I'm angry")
+elif i == "tired":
+    print("😞 I'm tired")
+elif i == "sleepy":
+    print("😴 Sleepy")
+else:
+    print("🤔 Interesting")
+    
